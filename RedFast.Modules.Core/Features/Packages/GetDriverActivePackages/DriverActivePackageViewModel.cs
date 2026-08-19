9aa43e58d0b3465cc5bd71d0ebbb6054b0e1b2bb
andreArce <andrearce343@gmail.com>
2026-08-18 23:58:20 -0300

    feat: implentada busca de pacotes ativos do entregador

---
+++ RedFast.Modules.Core/Features/Packages/GetDriverActivePackages/DriverActivePackageViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace RedFast.Modules.Core.Features.Packages.GetDriverActivePackages;
+
+public record DriverActivePackageViewModel
+(
+    Guid Id,
+    string TrackingCode,
+    string OriginAddress,
+    string DestinationAddress,
+    decimal Weight,
+    string CurrentStatus,
+    string SenderName
+    );
