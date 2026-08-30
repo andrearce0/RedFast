8d42a44c5695a81a2fda681b4ef8a33fa865c3c2
andreArce <andrearce343@gmail.com>
2026-08-29 22:53:01 -0300

    test: mudanca de status de pacotes

---
+++ RedFast.Modules.Core.Tests/Entities/PackageTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using RedFast.Modules.Core.Entities;
+using RedFast.Modules.Core.Entities.Enums;
+
+namespace RedFast.Modules.Core.Tests.Entities;
+
+public class PackageTests
+{
+    [Fact]
+    public void UpdateStatus_ToAwaitingPickup_FromCreated_ShouldUpdateStatusAndAddEvent()
+    {
+        var package = new Package
+        {
+            Id = Guid.NewGuid(),
+            TrackingCode = "TST-123456",
+            OriginAddress = "Rua A",
+            DestinationAddress = "Rua B",
+            Weight = 10.5m,
+            SenderId = Guid.NewGuid()
+        };
+
+        package.UpdateStatus(PackageStatus.AwaitingPickup, "Aguardando motorista", null);
+
+        package.CurrentStatus.Should().Be(PackageStatus.AwaitingPickup);
+        package.Events.Should().HaveCount(1);
+        package.Events.First().Status.Should().Be(PackageStatus.AwaitingPickup);
+    }
+
+    [Fact]
+    public void UpdateStatus_ToDelivered_FromCreated_ShouldThrowException()
+    {
+        var package = new Package
+        {
+            Id = Guid.NewGuid(),
+            TrackingCode = "TST-123456",
+            OriginAddress = "Rua A",
+            DestinationAddress = "Rua B",
+            Weight = 10.5m,
+            SenderId = Guid.NewGuid()
+        };
+
+        Action action = () => package.UpdateStatus(PackageStatus.Delivered, "Ação não permitida", null);
+
+        action.Should().Throw<InvalidOperationException>()
+            .WithMessage("*Transição de status inválida*");
+    }
+}
